e546b6ed449539eb92babe4a06b1677c80b0b01e
lsavoie <twosavoie@gmail.com>
2022-08-19 00:57:59 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    // navMenu.classList.add("hide"); // trying 
+    hamburger.setAttribute('aria-expanded', 'false');
+}
+
+hamburger.addEventListener('click', function () {
+    if (hamburger.classList.contains('active')) {
+        this.setAttribute('aria-expanded', 'false');
+        hamburger.classList.remove('active');
+        navMenu.classList.remove("active");
+    } else {
+        hamburger.classList.add('active');
+        navMenu.classList.toggle("active");
+        this.setAttribute('aria-expanded', 'true');
+    }
+});
